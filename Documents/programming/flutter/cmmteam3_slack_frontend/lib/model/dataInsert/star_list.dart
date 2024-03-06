f3802b71c3861ff6c6b357707e98472705984147
ThantsinKS <thantzinphyoe@mm.cybermissions.co.jp>
2024-03-06 19:37:05 +0900

    cmmit

--- Documents/programming/flutter/cmmteam3_slack_frontend/lib/model/dataInsert/star_list.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_frontend/model/StarLists.dart';
-
-class StarListStore {
-  static StarLists? starList;
-}
--- Documents/programming/flutter/cmmteam3_slack_frontend/lib/model/dataInsert/user_management_store.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_frontend/model/user_management.dart';
-
-class UserManagementStore {
-  static UserManagement? userManagement;
-}
